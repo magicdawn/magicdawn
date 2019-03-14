cf9594d63f283026b62b658a3be8eb57953f8a22
magicdawn <magicdawn@qq.com>
2019-03-15 01:08:39 +0800

    update at 2019-03-15 01:08:39

---
+++ interview/problems/simple-calculator/02.js
@@ -0,0 +1,67 @@
+const input = "100+31*5/6-2+3*6";
+const val = calc(input);
+console.log(val)
+
+function calc(s) {
+  let parts = s.split(/\b(?=[\+\-\*\/\d])/);
+  let stack = []
+  let stackLast = () => stack[stack.length -1]
+
+  const handle = (cur) => {
+    const math = {
+      '+': (a,b) => a + b,
+      '-': (a,b) => a - b,
+      '*': (a,b) => a * b,
+      '/': (a,b) => a / b,
+    }
+
+    const op = stackLast()
+    if(!math[op]) return
+
+    stack.pop() // remove op
+    let val = stack.pop() // left
+    val = math[op](val, cur) // do math(left, right)
+    stack.push(val)
+  }
+  
+
+  for (let p of parts) {
+    if (['*', '/', '+', '-'].includes(p)) {
+      stack.push(p)
+      continue
+    }
+
+    if (/\d+/.test(parts)) {
+      const n = Number(p)
+      if(['*', '/'].includes(stackLast())) {
+        handle(n)
+        continue
+      } else {
+        stack.push(n)
+      }
+    }
+  }
+
+  parts = stack
+  stack = []
+  stackLast = () => stack[stack.length - 1]
+  for (let p of parts) {
+    if (['+', '-'].includes(p)) {
+      stack.push(p)
+      continue
+    }
+
+    if (/\d+/.test(parts)) {
+      const n = Number(p)
+      if (['+', '-'].includes(stackLast())) {
+        handle(n)
+        continue
+      } else {
+        stack.push(n)
+      }
+    }
+  }
+
+  const val = stack.pop()
+  return val
+}
