b6714e4ec8c1f4f54272e1fbd4ea1e8b876a46ca
magicdawn <magicdawn@qq.com>
2019-03-17 15:29:02 +0800

    update at 2019-03-17 15:29:02

--- md/fe/service-worker/workbox/sw.js
+++ md/fe/service-worker/workbox/sw.js
@@ -1,12 +1,24 @@
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js'
-)
+// importScripts(
+//   'https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js'
+// )
+
+const version = 'latest'
+importScripts(`https://cdn.jsdelivr.net/npm/workbox-sw@${version}`)
+workbox.setConfig({
+  modulePathCb(name, debug) {
+    const env = debug ? 'dev' : 'prod'
+    return `https://cdn.jsdelivr.net/npm/${name}@${version}/build/${name}.${env}.js`
+  },
+})
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`)
 } else {
   console.log(`Boo! Workbox didn't load 😬`)
 }
+
+workbox.core.skipWaiting()
+workbox.core.clientsClaim()
 
 const route = workbox.routing.registerRoute
 const {
